e497788a9211bdba56dee8efde6cd1373a49b7d2
Erkan Ozcan <erkanozcan@Erkans-MacBook-Pro.local>
2021-04-21 11:49:32 -0500

    Added an observable and application updates with new post

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,19 +1,25 @@
 import { Injectable } from "@angular/core";
-import { timeLog } from "console";
 import { Post } from "./post.model";
+import { Subject } from 'rxjs';
 
 
-@Injectable({providedIn: 'root'})
+@Injectable()
 export class PostsService{
-    private posts: Post[];
+    private posts: Post[] = [];
+    private postsUpdated = new Subject<Post[]>();
     
     getPosts(){
         return [...this.posts];
     }
 
+    getPostUpdatedListener() {
+        return this.postsUpdated.asObservable();
+    }
+
     addPost(title: string, content: string){
         const post: Post = {title: title, content: content}
         this.posts.push(post);
+        this.postsUpdated.next([...this.posts]);
     }
 
 }
