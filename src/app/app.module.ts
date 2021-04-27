7a19d995ee5d729df083da6ddb28c6a2d99aaa64
Erkan Ozcan <erkanozcan@Erkans-MacBook-Pro.local>
2021-04-27 14:04:25 -0500

    Front-end and Back-end are connected

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'
-import { MatInputModule} from '@angular/material/input'
-import { MatCardModule} from '@angular/material/card'
-import { MatButtonModule} from '@angular/material/button'
-import { MatToolbarModule} from '@angular/material/toolbar'
-import { MatExpansionModule} from '@angular/material/expansion'
-
+import { MatInputModule } from '@angular/material/input'
+import { MatCardModule } from '@angular/material/card'
+import { MatButtonModule } from '@angular/material/button'
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatExpansionModule } from '@angular/material/expansion'
+import { HttpClientModule } from "@angular/common/http"
 
 
 import { AppRoutingModule } from './app-routing.module';
@@ -33,7 +33,8 @@ import { PostsService } from './posts/posts.service';
     MatCardModule,
     MatButtonModule,
     MatToolbarModule,
-    MatExpansionModule  
+    MatExpansionModule,
+    HttpClientModule  
   ],
   providers: [PostsService],
   bootstrap: [AppComponent]
--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -23,8 +23,9 @@ export class PostListComponent implements OnInit, OnDestroy {
    }
 
    ngOnInit() {
-        this.posts = this.postsService.getPosts();
-        this.postsSub = this.postsService.getPostUpdatedListener().subscribe((posts: Post[]) => {
+        this.postsService.getPosts();
+        this.postsSub = this.postsService.getPostUpdatedListener()
+        .subscribe((posts: Post[]) => {
             this.posts = posts;
         });
 }
