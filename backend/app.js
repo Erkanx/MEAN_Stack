2972f24420f8a8e3f47fc1b1f2a61bd640bb8f3c
Erkan Ozcan <erkanozcan@Erkans-MacBook-Pro.local>
2021-05-06 14:02:25 -0500

    Fetching data from database

--- backend/app.js
+++ backend/app.js
@@ -34,27 +34,18 @@ app.post("/api/posts", (req, res, next) =>{
         title: req.body.title,
         content: req.body.content
     });
-    console.log(post);
+   post.save();
     res.status(201).json({
         message: 'Post added succesful'
     });
 });
 
 app.get('/api/posts', (req, res, next) => {
-    const posts = [
-    {   id: 'Test12', 
-        title: 'First server-side post', 
-        content: "This is coming from the server"
-    },
-    {   id: 'Woohoo92', 
-        title: 'Second server-side post', 
-        content: "This is coming from the server!@"
-    },
-
-    ]
-    res.status(200).json({
-        message: 'Post fetched successfully!',
-        posts: posts
+    Post.find().then(documents => {
+        res.status(200).json({
+            message: 'Post fetched successfully!',
+            posts: documents
+        });
     });
 });
 
