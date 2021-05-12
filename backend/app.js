2eb9488f02dbc1e6ab1434e2754ad4ce792af184
Erkan Ozcan <erkanozcan@Erkans-MacBook-Pro.local>
2021-05-12 11:00:47 -0500

    Styled the header, edit button is now working and connected with the backend

--- backend/app.js
+++ backend/app.js
@@ -24,7 +24,7 @@ app.use((req, res, next) => {
     );
     res.setHeader(
     "Access-Control-Allow-Methods", 
-    "GET, POST, PATCH, DELETE, OPTIONS"
+    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
     );
     next();
 })
@@ -42,6 +42,18 @@ app.post("/api/posts", (req, res, next) =>{
    });
 });
 
+app.put("/api/posts/:id", (req, res, next) => {
+    const post = new Post({
+        _id: req.body.id,
+        title: req.body.title,
+        content: req.body.content
+    })
+    Post.updateOne({_id: req.params.id}, post).then(result => {
+        console.log(result)
+        res.status(200).json({message: 'Update Successful!'})
+    })
+})
+
 app.get('/api/posts', (req, res, next) => {
     Post.find().then(documents => {
         res.status(200).json({
@@ -50,6 +62,16 @@ app.get('/api/posts', (req, res, next) => {
         });
     });
 });
+
+app.get("/api/posts/:id", (req, res, next) => {
+    Post.findById(req.params.id).then(post => {
+        if(post){
+            res.status(200).json(post);
+        } else {
+            res.status(404).json({message: 'Post not found'});
+        }
+    });
+})
 
 //Keynote: Inside the parenthesis, the order has to be (req, res, next)!!
 app.delete("/api/posts/:id", (req, res, next ) => {
