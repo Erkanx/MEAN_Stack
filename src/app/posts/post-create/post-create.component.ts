facf9fddaf8b497dca33febcc5c373b9aaff6dde
Erkan Ozcan <erkanozcan@Erkans-MacBook-Pro.local>
2021-05-13 10:11:33 -0500

    Added a validator for the image uploads

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit} from "@angular/core";
 import { FormControl, FormGroup, Validators } from "@angular/forms";
 import { ActivatedRoute, ParamMap } from "@angular/router";
 import { Post } from "../post.model";
+import { mimeType } from "./mime-type.validator";
 
 import { PostsService } from "../posts.service";
 
@@ -35,7 +36,7 @@ export class PostCreateComponent implements OnInit {
             content: new FormControl(null, {
                 validators: [Validators.required]
             }),
-            image: new FormControl(null, {validators: [Validators.required]
+            image: new FormControl(null, {validators: [Validators.required], asyncValidators: [mimeType]
             })
         });
         this.route.paramMap.subscribe((paramMap: ParamMap) => {
@@ -65,7 +66,7 @@ export class PostCreateComponent implements OnInit {
     onImagePicked(event: Event){
         const file = (event.target as HTMLInputElement).files[0];
         this.form.patchValue({image: file});
-        this.form.get('image').updateValueAndValidity();
+        this.form.get("image").updateValueAndValidity();
         const reader = new FileReader();
         reader.onload = () => {
             this.imagePreview = reader.result as string;
